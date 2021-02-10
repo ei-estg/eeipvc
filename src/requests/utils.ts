42115ad8d617a216728154e2e230a57b4cfb70c9
André Sousa <andrematosdesousa@gmail.com>
2021-02-10 21:33:36 +0000

    Added missing file on previous commit

---
+++ src/requests/utils.ts
@@ -0,0 +1,49 @@
+import fetch from 'node-fetch'
+
+interface Counties {
+    total: number
+    north: number
+    center: number
+    alentejo: number
+    algarve: number
+    acores: number
+    madeira: number
+    foreign?: number
+}
+
+interface CountiesConfirmed extends Counties {
+    new: number
+}
+
+interface CovidEntry {
+    confirmed: CountiesConfirmed
+    recovered: number
+    deaths: number
+}
+
+export const getCovidInPortugalForDate = async (date1: string) => {
+    let req = await fetch(
+        `https://covid19-api.vost.pt/Requests/get_entry/${date1}`,
+    )
+    if (!req.ok) return null
+
+    let data = await req.json()
+    let key: number = parseInt(Object.keys(data.data)[0])
+
+    let reData: CovidEntry = {
+        confirmed: {
+            total: data['confirmados'][key],
+            new: data['confirmados_novos'][key],
+            north: data['confirmados_arsnorte'][key],
+            center: data['confirmados_arscentro'][key],
+            alentejo: data['confirmados_arsalentejo'][key],
+            algarve: data['confirmados_arsalgarve'][key],
+            acores: data['confirmados_acores'][key],
+            madeira: data['confirmados_madeira'][key],
+        },
+        recovered: data['recuperados'][key],
+        deaths: data['obitos'][key],
+    }
+
+    return reData
+}
