28b88601457569b3dc81bda00897fbe3e427a4b1
Marco Porto <50577988+4SMarcoPorto@users.noreply.github.com>
2021-04-29 16:39:36 +0100

    Update answer.ts

--- src/commands/fun/answer.ts
+++ src/commands/fun/answer.ts
@@ -17,7 +17,7 @@ export const answerCommand: Command = {
     },
     async run(message: Message, { shift }) {
         let roles = await (message.channel as GuildChannel).guild.roles.fetch()
-        let role = await (message.channel as GuildChannel).roles.cache.find(role => role.name == 'Turma ' + shift)
+        let role = await (message.channel as GuildChannel).guild.roles.cache.find(role => role.name == 'Turma ' + shift)
         if (!role) return 'Turma não existe'
         if (!role.members) return 'Sem utilizadores encontrados'
         return `${role.members.random()} é a tua vez de responder :D`
