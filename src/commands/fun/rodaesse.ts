73d4faca14ce638a01286b6dbd610c40a25b2825
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-06-29 15:14:53 +0100

    new urls

--- src/commands/fun/rodaesse.ts
+++ src/commands/fun/rodaesse.ts
@@ -1,4 +1,5 @@
 import { Message,MessageAttachment } from 'discord.js'
+
 import { Command } from '../Command'
 const fs = require('fs');
 export const rodaEsse: Command = {
@@ -6,7 +7,12 @@ export const rodaEsse: Command = {
 	description: 'Envia um charuto virtual para os que precisam descomprimir, ou para depois das aalas do Ribeiro.',
 
 	async run(message: Message) {
-		const attachment = new MessageAttachment('https://64.media.tumblr.com/tumblr_ly5tbgg9yj1qert2ho1_500.gif');
+
+		const urlPool=["https://media0.giphy.com/media/UfjQ4GvNMHlf1lBhOW/giphy.gif",
+		'https://64.media.tumblr.com/tumblr_ly5tbgg9yj1qert2ho1_500.gif',
+		'https://thumbs.gfycat.com/SomeTanAfricanpiedkingfisher-small.gif',
+	'https://66.media.tumblr.com/521a199d71761032d3d6526b173063a7/tumblr_ooef1eyDxa1u521p1o1_500.gif']
+		const attachment = new MessageAttachment(urlPool[Math.floor(Math.random() * urlPool.length)]);
 		await message.channel.send(attachment)
 		
 		return undefined
