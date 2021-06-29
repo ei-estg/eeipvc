b862eb3eb5b52c7dfe49576732309ccba70d02ee
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-06-29 15:01:36 +0100

    finnaly i hope

--- src/commands/fun/rodaesse.ts
+++ src/commands/fun/rodaesse.ts
@@ -1,12 +1,13 @@
 import { Message,MessageAttachment } from 'discord.js'
 import { Command } from '../Command'
-
+const fs = require('fs');
 export const rodaEsse: Command = {
 	name: 'rodaesse',
 	description: 'Envia um charuto virtual para os que precisam descomprimir, ou para depois das aalas do Ribeiro.',
 
 	async run(message: Message) {
-		await message.channel.send(new MessageAttachment('../../data/joint.mp4'))
+		const attachment = new MessageAttachment('https://tenor.com/view/weed-the-virtual-joint-smoke-gif-12178837');
+		await message.channel.send(attachment)
 		
 		return undefined
 	}
