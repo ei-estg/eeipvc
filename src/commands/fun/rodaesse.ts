c797159bd4dc34ebfcec1feac22261c3dadac0f9
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-06-29 14:52:17 +0100

    typo

--- src/commands/fun/rodaesse.ts
+++ src/commands/fun/rodaesse.ts
@@ -3,7 +3,7 @@ import { Command } from '../Command'
 
 export const rodaEsse: Command = {
 	name: 'rodaesse',
-	description: 'Envia um charuto virtual para os que precisam descomprimir, ou para depois das aualas do Ribeiro.',
+	description: 'Envia um charuto virtual para os que precisam descomprimir, ou para depois das aalas do Ribeiro.',
 
 	async run(message: Message) {
 		await message.channel.send(new MessageAttachment('../../data/joint.mp4'))
