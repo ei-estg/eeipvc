f53b4bdb74019bf9c386bddc2d6367eb91794d91
André Sousa <andrematosdesousa@gmail.com>
2021-07-15 16:03:23 +0100

    Forgot to commit a file

---
+++ src/private/movies_timmer_handler.ts
@@ -0,0 +1,28 @@
+import { Cinema, getMoviesByLocation } from '../../lib/cineplace'
+import { eiEmbed } from '../defaults/embed'
+import { TextChannel } from 'discord.js'
+import moment from 'moment'
+
+export const moviesTimmerHander = async (channel: TextChannel) => {
+    const messages = await channel.messages.fetch()
+
+    messages.forEach(message => message.delete())
+
+    const movies = await getMoviesByLocation(Cinema.EstacaoViana)
+    movies.forEach(movie => {
+        const embed = eiEmbed()
+        embed.setThumbnail(movie.imageUrl)
+        embed.setTitle(movie.name)
+        embed.addFields({
+            name: 'Horários',
+            value: movie.dateTimes.map(dateTime => `${moment(dateTime.date, 'YYYY-MM-DD').format('DD-MM-YYYY')} - ${dateTime.times.join(' | ')}`),
+        })
+        channel.send({ embed })
+    })
+
+    const currentMomentTime = moment()
+    await channel.edit({
+        topic: `Filmes a serem exibidos no Cineplace - Estação Viana. Atualizado no dia ${currentMomentTime.format('DD-MM-YYYY')} às ${currentMomentTime.format('HH:mm')}`
+    })
+    console.log(currentMomentTime)
+}
