ad88e40cb5c675863cea0c73008407dee05d74bf
João Martinho <74744920+Varrys@users.noreply.github.com>
2021-09-29 20:51:49 +0100

    Update para listagem de pisos individual. !piso 1

---
+++ src/commands/classroom/piso.ts
@@ -0,0 +1,32 @@
+import { Message } from 'discord.js'
+import { Command } from '../Command'
+
+export const pisoCommand: Command = {
+    name: 'piso',
+    description: 'Envia a planta com as salas de cada piso da ESTG.',
+    args: {
+        floor: {
+            text: 'piso',
+            example: '1',
+            check: () => true,
+        },
+    },
+
+    async run(message: Message, {floor}) {
+        await message.channel.send("Piso ***",{floor},"***. Não te percas novamente :D (Roda o telemovel)")
+        if (floor == 1) {
+            await message.channel.send("https://i.imgur.com/8ZkWMat.png%22")
+        }
+        else if (floor == 2) {
+        await message.channel.send("https://i.imgur.com/ohD8429.png%22")
+        }
+        else if (floor == 3) {
+        await message.channel.send("https://i.imgur.com/zrrdvcP.png%22")
+        }
+        else {
+            await message.channel.send("Comando inválido. Ex: !piso 2")
+        }
+    
+        return undefined
+    }
+}
