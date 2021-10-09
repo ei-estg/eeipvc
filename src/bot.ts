be5dd76f77dddff8379077e620deececd773cb1a
Marco Porto <50577988+marco-porto@users.noreply.github.com>
2021-10-09 12:36:37 +0100

    fix welcome message

--- src/bot.ts
+++ src/bot.ts
@@ -32,7 +32,9 @@ import { moviesTimmerHander } from './private/movies_timmer_handler'
 import { minecraftCommand } from './commands/fun/minecraft'
 import { minecraftTimmerHandler } from './private/minecraft_timmer_handler'
 
-const bot = new BotClient(botConfig, { partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER'] })
+const bot = new BotClient(botConfig, {
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER'],
+})
 
 let guildMembersCount = 0
 
@@ -51,7 +53,6 @@ bot.on('ready', async () => {
 bot.on('guildMemberAdd', async () => {
     guildMembersCount++
     await updateStatus()
-
 })
 
 bot.on('guildMemberRemove', async () => {
@@ -85,48 +86,72 @@ bot.handlers.commands.register(
     roastCoder,
     verifyCommand,
     minecraftCommand,
-    pisoCommand
+    pisoCommand,
 )
 
-bot.on("guildMemberAdd", async (member) => {
-  const welcomeChannel = member.guild.channels.cache.find(channel => channel.id == '766278332500803610');
-  welcomeChannel.send(`${member.user} acabou de entrar.`);  
-});
+bot.on('guildMemberAdd', async (member: any) => {
+    const channel = member.guild.channels.cache.find(
+        (channel) => channel.id == '766278332500803610',
+    )
+    channel.send(
+        `Boas ${
+            member.user
+        }. Dá uma olhadela nas salas ${member.guild.channels.cache
+            .get('779437283966189618')
+            .toString()} e ${member.guild.channels.cache
+            .get('779491420079259659')
+            .toString()} para ficares a conhecer as regras e ainda acederes a diferentes áreas do servidor.`,
+    )
+})
 
 const getChannelById = async (guildId: string, channelId: string) => {
     const guild = await bot.guilds.fetch(guildId)
-    return guild.channels.cache.get(
-        channelId,
-    ) as TextChannel
+    return guild.channels.cache.get(channelId) as TextChannel
 }
 
 // CronJobs
 bot.handlers.timers.register({
     cronTime: botConfig.timmers.meals.cronTime,
-    channel: () => getChannelById(botConfig.guild.id, botConfig.timmers.meals.channelId),
-    handler: () => mealsCommand.run(undefined, {
-        date: moment().add(1, 'day').format('YYYY-MM-DD'),
-    }),
+    channel: () =>
+        getChannelById(botConfig.guild.id, botConfig.timmers.meals.channelId),
+    handler: () =>
+        mealsCommand.run(undefined, {
+            date: moment().add(1, 'day').format('YYYY-MM-DD'),
+        }),
 })
 
 bot.handlers.timers.register({
     cronTime: botConfig.timmers.cineplace.cronTime,
-    channel: () => getChannelById(botConfig.guild.id, botConfig.timmers.cineplace.channelId),
+    channel: () =>
+        getChannelById(
+            botConfig.guild.id,
+            botConfig.timmers.cineplace.channelId,
+        ),
     handler: async () => {
         return moviesTimmerHander(
-            await getChannelById(botConfig.guild.id, botConfig.timmers.cineplace.channelId)
+            await getChannelById(
+                botConfig.guild.id,
+                botConfig.timmers.cineplace.channelId,
+            ),
         )
     },
 })
 
 bot.handlers.timers.register({
     cronTime: botConfig.timmers.minecraft.cronTime,
-    channel: () => getChannelById(botConfig.guild.id, botConfig.timmers.minecraft.channelId),
+    channel: () =>
+        getChannelById(
+            botConfig.guild.id,
+            botConfig.timmers.minecraft.channelId,
+        ),
     handler: async () => {
         return minecraftTimmerHandler(
-            await getChannelById(botConfig.guild.id, botConfig.timmers.minecraft.channelId)
+            await getChannelById(
+                botConfig.guild.id,
+                botConfig.timmers.minecraft.channelId,
+            ),
         )
-    }
+    },
 })
 
 // Reactions
