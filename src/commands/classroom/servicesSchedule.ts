36ac210c9f1222b61badeddefce185cd3edc49f0
João Martinho <74744920+Varrys@users.noreply.github.com>
2021-10-11 20:02:17 +0100

    Add files via upload

---
+++ src/commands/classroom/servicesSchedule.ts
@@ -0,0 +1,22 @@
+import { Command } from '../Command'
+import ServicesSchedule from '../../../data/services-schedule.json'
+import { eiEmbed } from '../../defaults/embed'
+
+export const servicesCommand: Command = {
+    name: 'serviços',
+    alias: ['servicos'],
+    description: 'Horário dos vários serviços no IPVC',
+    async run() {
+        const servicesEmbed = eiEmbed().setTitle('⏰ Horários dos Serviços IPVC')
+
+         ServicesSchedule.service.forEach((service) => {
+            servicesEmbed.addFields({  
+                name: `**${service.name}**`,              
+                value: service.schedule,
+            })            
+        })
+        servicesEmbed.setFooter("Poderá ocorrer alterações nos hórarios")
+        return servicesEmbed
+    },
+
+}
