8fd565dc8bbc0e30ed735e7d646c14c584baec8d
André Sousa <andrematosdesousa@gmail.com>
2021-10-11 10:29:57 +0100

    Fix schedules

--- lib/on-ipvc/schedule.ts
+++ lib/on-ipvc/schedule.ts
@@ -4,122 +4,121 @@ import moment from 'moment'
 import { CookieHeader, Schedule } from './index'
 
 const parseSchedulesHtmlContent = (content: string) => {
-	const match = content.match(/events_data\s=\s(.+);/gm)
-	if (!match) return null
+    const match = content.match(/events_data\s=\s(.+);/gm)
+    if (!match) return null
 
-	const data = match[0].replace('events_data = ', '')
-	return eval(data)
+    const data = match[0].replace('events_data = ', '')
+    return eval(data)
 }
 
 export const getSchedule = async (
-	login: any,
-	year: string,
-	semester: string,
-	klass: string,
-	week: number
+    login: any,
+    year: string,
+    semester: string,
+    klass: string,
+    week: number,
 ) => {
-	const form = new FormData()
-	form.append('param_anoletivoH', year)
-	form.append('param_semestreH', semester)
-	form.append('param_turmaH', klass)
-	form.append('param_semanaH', week)
-	form.append('emissorH', 'consultageral')
+    const form = new FormData()
+    form.append('param_anoletivoH', year)
+    form.append('param_semestreH', semester)
+    form.append('param_turmaH', klass)
+    form.append('param_semanaH', week)
+    form.append('emissorH', 'consultageral')
 
-	const res = await fetch(
-		'https://on.ipvc.pt/v1/modulos/atividadeletiva/horario_source_v3.php',
-		{
-			method: 'post',
-			headers: login,
-			body: form
-		}
-	)
+    const res = await fetch(
+        'https://on.ipvc.pt/v1/modulos/atividadeletiva/horario_source_v3.php',
+        {
+            method: 'post',
+            headers: login,
+            body: form,
+        },
+    )
 
-	const html = await res.text()
-	const parsedContent = parseSchedulesHtmlContent(html)
+    const html = await res.text()
+    const parsedContent = parseSchedulesHtmlContent(html)
 
-	if (!parsedContent) return null
+    if (!parsedContent) return null
 
-	const reParsedContent: Schedule = []
+    const reParsedContent: Schedule = []
 
-	const DATE_FORMAT = 'YYYY-MM-DD HH:mm:SS'
+    const DATE_FORMAT = 'YYYY-MM-DD HH:mm:SS'
 
-	parsedContent.forEach((item: any) => {
-		const classRoom = item.title.split(' - ')[1]
-		let id, className, classShortName, __, classType, temp
+    parsedContent.forEach((item: any) => {
+        const classRoom = item.title.split(' - ')[1]
+        let id, className, classShortName, __, classType, temp
 
-		if (item.datauc.includes('|')) {
-			;[id, className, temp] = item.datauc.split(' | ')
-			;[classShortName, __, classType] = temp.split(' - ')
-		} else {
-			;[id, className, classShortName, __, classType] = item.datauc.split(
-				'-'
-			)
-		}
+        if (item.datauc.includes('|')) {
+            ;[id, className, temp] = item.datauc.split(' | ')
+            ;[classShortName, __, classType] = temp.split(' - ')
+        } else {
+            ;[id, className, classShortName, __, classType] =
+                item.datauc.split('-')
+        }
 
-		reParsedContent.push({
-			start: moment(item.datadatainicio, DATE_FORMAT).unix(),
-			end: moment(item.datadatafim, DATE_FORMAT).unix(),
-			lesson: {
-				name: className,
-				shortName: classShortName,
-				type: classType,
-				classRoom
-			},
-			id: parseFloat(id),
-			teacher: item.datadocentes
-				.replace(/<.+; /, '')
-				.replace('</div>', ''),
-			status:
-				item.color === '#7f5555'
-					? 'REPLACED'
-					: item.color === '#ff0000'
-					? 'CANCELED'
-					: item.color === '#f0a0a0'
-						? 'NOT_TAUGHT'
-						: 'OTHER'
-		})
-	})
+        reParsedContent.push({
+            start: moment(item.datadatainicio, DATE_FORMAT).unix(),
+            end: moment(item.datadatafim, DATE_FORMAT).unix(),
+            lesson: {
+                name: className,
+                shortName: classShortName,
+                type: classType,
+                classRoom,
+            },
+            id: parseFloat(id),
+            teacher: item.datadocentes
+                .replace(/<.+; /, '')
+                .replace('</div>', ''),
+            status:
+                item.color === '#7f5555'
+                    ? 'REPLACED'
+                    : item.color === '#ff0000'
+                    ? 'CANCELED'
+                    : item.color === '#f0a0a0'
+                    ? 'NOT_TAUGHT'
+                    : 'OTHER',
+        })
+    })
 
-	return reParsedContent
+    return reParsedContent
 }
 
 export const getScheduleByDate = async (
-	login: CookieHeader,
-	scheduleYear: string,
-	semester: string,
-	klass: string,
-	year: string,
-	month: string,
-	day: string
+    login: CookieHeader,
+    scheduleYear: string,
+    semester: string,
+    klass: string,
+    year: string,
+    month: string,
+    day: string,
 ) => {
-	const STATIC_DATE = 355
-	const staticMomentTime = moment({
-		year: 2021,
-		day: 29,
-		month: 9
-	})
-	const requestTime = moment({
-		year: parseInt(year),
-		day: parseInt(day),
-		month: parseInt(month)
-	})
+    const STATIC_DATE = 355
+    const staticMomentTime = moment({
+        year: 2021,
+        day: 28,
+        month: 9,
+    })
+    const requestTime = moment({
+        year: parseInt(year),
+        day: parseInt(day),
+        month: parseInt(month),
+    })
 
-	const scheduleWeek = requestTime.diff(staticMomentTime, 'weeks')
+    const scheduleWeek = requestTime.diff(staticMomentTime, 'weeks')
 
-	const weekSchedule = await getSchedule(
-		login,
-		scheduleYear,
-		semester,
-		klass,
-		STATIC_DATE + scheduleWeek
-	)
-	if (!weekSchedule) {
-		return null
-	}
+    const weekSchedule = await getSchedule(
+        login,
+        scheduleYear,
+        semester,
+        klass,
+        STATIC_DATE + scheduleWeek,
+    )
+    if (!weekSchedule) {
+        return null
+    }
 
-	return weekSchedule.filter(
-		(item) =>
-			moment.unix(item.start).format('DD-MM-YYYY') ===
-			`${day}-${month}-${year}`
-	)
+    return weekSchedule.filter(
+        (item) =>
+            moment.unix(item.start).format('DD-MM-YYYY') ===
+            `${day}-${month}-${year}`,
+    )
 }
