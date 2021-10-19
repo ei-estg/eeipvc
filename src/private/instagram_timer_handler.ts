d2b31a1c5b9c3ad1bfcd23517250462bcf73ac91
André Sousa <andrematosdesousa@gmail.com>
2021-10-19 11:27:46 +0100

    Added missing file

---
+++ src/private/instagram_timer_handler.ts
@@ -0,0 +1,39 @@
+import { eiEmbed } from '../defaults/embed'
+import { TextChannel } from 'discord.js'
+
+const ig = require('instagram-scraping')
+
+export const instagramTimerHandler = async (
+    channel: TextChannel,
+    accountUsername: string,
+) => {
+    const userPage = await ig.scrapeUserPage(accountUsername)
+
+    const messages = await channel.messages.fetch()
+    const botMessages = messages.filter((msg) => msg.author.bot)
+    const numberOfPosts = botMessages.size
+    console.log('nOp', numberOfPosts)
+
+    if (numberOfPosts < userPage.medias.length) {
+        const diff = userPage.medias.length - numberOfPosts
+
+        const promises: Promise<any>[] = []
+        for (let i = diff - 1; i >= 0; i--) {
+            const embed = eiEmbed()
+
+            embed.setTitle(userPage.user.full_name)
+            embed.setThumbnail(userPage.user.profile_pic_url)
+            embed.setImage(userPage.medias[i].node.display_url)
+            if (userPage.medias[i].node.location) {
+                embed.setFooter(userPage.medias[i].node.location.name)
+            }
+
+            promises.push(
+                channel.send({
+                    embed,
+                }),
+            )
+        }
+        await Promise.all(promises)
+    }
+}
