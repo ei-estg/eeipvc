8e5a53086dfbdacab11e61f5d9880fcd29a09bf4
João Martinho <74744920+Varrys@users.noreply.github.com>
2021-10-20 00:21:07 +0100

    final fix

--- src/commands/classroom/servicesSchedule.ts
+++ src/commands/classroom/servicesSchedule.ts
@@ -14,7 +14,10 @@ export const servicesCommand: Command = {
         ServicesSchedule.service.forEach((service) => {
             servicesEmbed.addFields({
                 name: `**${service.name}**`,
-                value: `${service.schedule} \n email: ${service.email} \n telefone: ${service.phone}`,
+                value: `service.schedule`,
+                value: `Email: service.email`,
+                value: `Telefone: service.phone`,
+                
             })
         })
         servicesEmbed.setFooter('Poderá ocorrer alterações nos hórarios')
