a9eb4eadfa015041a88af6bc8eee0e89ecd42962
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-11-04 15:28:32 +0000

    wtf

--- src/commands/fun/stock.ts
+++ src/commands/fun/stock.ts
@@ -1,6 +1,7 @@
 import { Command } from '../Command'
 import { getStock } from '../../requests/stock'
 import { Message } from 'discord.js'
+import { eiEmbed } from '../../defaults/embed'
 
 export const stockCommand: Command = {
     name: 'stock',
@@ -17,15 +18,15 @@ export const stockCommand: Command = {
     async run(message: Message, { symbol }): Promise<any> {
         try {
             let stock = await getStock(symbol)
-            console.log(stock)
-
             if (!stock) {
                 return message.channel.send(
                     'Este stock não existe!. Ex: !stock AAPL',
                 )
             }
+            const stockEmbed = eiEmbed().setTitle(stock.displayName);
+            stockEmbed.setFooter(`Ultima preço: ${stock.regularMarketPrice} ${stock.currency}`)
 
-            return message.channel.send("Última preço da "+stock.displayName+": "+stock.price+" "+stock.currency);
+            return stockEmbed;
         } catch (err) {
             console.error(err)
         }
