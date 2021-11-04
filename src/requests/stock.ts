6d30ddf574fde07da13729a8e9ec331922481383
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-11-04 15:17:17 +0000

    maneira à bruta

--- src/requests/stock.ts
+++ src/requests/stock.ts
@@ -7,19 +7,21 @@ export interface Stock {
 }
 
 export const getStock = async (stockTicker: string) => {
-
-      let req = await fetch(`https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v6/finance/quote?symbols=${stockTicker}&lang=EN&region=ES`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "stock-data-yahoo-finance-alternative.p.rapidapi.com",
-            "x-rapidapi-key": "2ffdf1e29emsh56e8402b31fb1d1p13dec8jsn7c752aa7499b"
-        }
-    })
+    let req = await fetch(
+        `https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v6/finance/quote?symbols=${stockTicker}`,
+        {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-host':
+                    'stock-data-yahoo-finance-alternative.p.rapidapi.com',
+                'x-rapidapi-key':
+                    '2ffdf1e29emsh56e8402b31fb1d1p13dec8jsn7c752aa7499b',
+            },
+        },
+    )
 
     let response = await req.json()
-    let reData: Stock[] = []
+    let reData = await response.quoteResponse.result[0]
 
-    reData.push(response.quoteResponse.result[0])
-    
     return reData
 }
