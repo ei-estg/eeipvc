6ae782c4fddfd22cdbdd1aa02177e29f8d7a5890
André Sousa <andrematosdesousa@gmail.com>
2022-10-27 01:16:29 +0100

    Ran linter

--- apps/bot/src/config/environment.schema.ts
+++ apps/bot/src/config/environment.schema.ts
@@ -1,5 +1,5 @@
 import Joi from 'joi'
 
 export const environmentSchema = Joi.object({
-  DISCORD_BOT_TOKEN: Joi.string().required()
+    DISCORD_BOT_TOKEN: Joi.string().required(),
 }).unknown(true)
